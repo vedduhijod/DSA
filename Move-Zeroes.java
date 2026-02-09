f57edf9619cb2d7cd9147cc6c18251921617634a
Ved Duhijod <65458338+vedduhijod@users.noreply.github.com>
2026-02-09 22:53:21 +0530

    [283] [Time Beats: 90.02%] [Memory Beats: 16.36%] - LeetPush

--- Move-Zeroes.java
+++ Move-Zeroes.java
@@ -1,17 +1,14 @@
 1class Solution {
 2    public void moveZeroes(int[] nums) {
-3        int index =0;
-4        for(int i = 0; i < nums.length; i++){
-5            if(nums[i] != 0){
-6                nums[index] = nums[i];
-7                index++;
-8            }
-9        }
-10
-11        for(int i = index; i < nums.length; i++){
-12            nums[i] = 0;
-13            
-14        }
-15
-16    }
-17}
+3        int index = 0;
+4        for (int i = 0; i < nums.length; i++) {
+5            if (nums[i] != 0) {
+6                int temp = nums[i];
+7                nums[i] = nums[index];
+8                nums[index] = temp;
+9                index++;
+10            }
+11        }
+12    }
+13}
+14
