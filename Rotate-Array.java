9396a3e6022a8b21ca403d178e1f21e822d2ee76
Ved Duhijod <65458338+vedduhijod@users.noreply.github.com>
2026-02-10 01:22:30 +0530

    [189] [Time Beats: 100.00%] [Memory Beats: 92.75%] - LeetPush

--- Rotate-Array.java
+++ Rotate-Array.java
@@ -1,14 +1,21 @@
 1class Solution {
 2    public void rotate(int[] nums, int k) {
-3        int len = nums.length;
-4        int[] result = new int[len];
+3        int n = nums.length;
+4        k = k % n;  // important when k > n
 5
-6        for(int i = 0; i < len; i++){
-7            result[(i + k) % len] = nums[i];
-8        }
-9
-10        for(int i = 0; i < len; i++){
-11            nums[i] = result[i];
-12        }
-13    }
-14}
+6        reverse(nums, 0, n - 1);
+7        reverse(nums, 0, k - 1);
+8        reverse(nums, k, n - 1);
+9    }
+10
+11    private void reverse(int[] nums, int left, int right) {
+12        while (left < right) {
+13            int temp = nums[left];
+14            nums[left] = nums[right];
+15            nums[right] = temp;
+16            left++;
+17            right--;
+18        }
+19    }
+20}
+21
