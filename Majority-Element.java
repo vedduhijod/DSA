2f66f13404b8b086c0952ca422ac2d729353db54
Ved Duhijod <65458338+vedduhijod@users.noreply.github.com>
2026-02-13 03:08:30 +0530

    [169] [Time Beats: 99.83%] [Memory Beats: 16.51%] - LeetPush

--- Majority-Element.java
+++ Majority-Element.java
@@ -1,23 +1,20 @@
-1import java.util.HashMap;
-2
-3class Solution {
-4    public int majorityElement(int[] nums) {
-5        HashMap<Integer, Integer> map = new HashMap<>();
-6        int n = nums.length;
-7
-8        // count frequency
-9        for (int num : nums) {
-10            map.put(num, map.getOrDefault(num, 0) + 1);
-11        }
-12
-13        // find majority
-14        for (int key : map.keySet()) {
-15            if (map.get(key) > n / 2) {
-16                return key;
-17            }
-18        }
+1class Solution {
+2    public int majorityElement(int[] nums) {
+3        int cnt = 0;
+4        int el = 0;
+5        for(int i = 0; i < nums.length; i++){
+6            if(cnt == 0){
+7                cnt = 1;
+8                el = nums[i];
+9            }
+10            else if(nums[i] == el){
+11                cnt++;
+12            }
+13            else {
+14                cnt--;
+15            }
+16        }
+17        return el;
+18    }
 19
-20        return -1;
-21    }
-22}
-23
+20    }
