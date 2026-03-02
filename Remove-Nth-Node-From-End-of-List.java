3ce3d63cd0e9ab08d925254230a4dd1851cee3b4
Ved Duhijod <65458338+vedduhijod@users.noreply.github.com>
2026-03-02 23:02:19 +0530

    [19] [Time Beats: 100.00%] [Memory Beats: 89.74%] - LeetPush

--- Remove-Nth-Node-From-End-of-List.java
+++ Remove-Nth-Node-From-End-of-List.java
@@ -4,32 +4,28 @@
 4        // Edge case: empty list
 5        if (head == null) return null;
 6
-7        // Step 1: Count total nodes
-8        int cnt = 0;
-9        ListNode temp = head;
-10
-11        while (temp != null) {
-12            cnt++;
-13            temp = temp.next;
-14        }
-15
-16        // Step 2: If deleting head
-17        if (cnt == n) {
-18            return head.next;
-19        }
-20
-21        // Step 3: Find node before target
-22        int res = cnt - n;
-23        temp = head;
-24
-25        while (res > 1) {
-26            temp = temp.next;
-27            res--;
-28        }
-29
-30        // Step 4: Delete node
-31        temp.next = temp.next.next;
-32
-33        return head;
-34    }
-35}
+7        ListNode fast = head;
+8        ListNode slow = head;
+9
+10        // Move fast pointer n steps ahead
+11        for (int i = 0; i < n; i++) {
+12            fast = fast.next;
+13        }
+14
+15        // If fast becomes null, we need to delete the head
+16        if (fast == null) {
+17            return head.next;
+18        }
+19
+20        // Move both pointers together
+21        while (fast.next != null) {
+22            fast = fast.next;
+23            slow = slow.next;
+24        }
+25
+26        // Delete nth node from end
+27        slow.next = slow.next.next;
+28
+29        return head;
+30    }
+31}
