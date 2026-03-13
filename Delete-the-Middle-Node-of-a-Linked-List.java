852735617691c9251158861bc4d3efbe7c1b2c12
Ved Duhijod <65458338+vedduhijod@users.noreply.github.com>
2026-03-13 11:25:49 +0530

    [2095] [Time Beats: 99.68%] [Memory Beats: 20.04%] - LeetPush

--- Delete-the-Middle-Node-of-a-Linked-List.java
+++ Delete-the-Middle-Node-of-a-Linked-List.java
@@ -10,26 +10,22 @@
 10 */
 11class Solution {
 12    public ListNode deleteMiddle(ListNode head) {
-13        if(head == null || head.next == null) return null;      
-14        ListNode temp = head;
-15        int count = 0;
+13
+14        if (head == null || head.next == null)
+15            return null;
 16
-17        while(temp != null){
-18            temp = temp.next;
-19            count++;
-20        }
-21        int ans = count/2;
-22        temp = head;
-23while(temp != null){
-24    ans--;
-25
-26    if(ans == 0 && temp.next != null){
-27        temp.next = temp.next.next;
-28        break;
-29    }
-30
-31    temp = temp.next;
-32}
-33        return head;
-34    }
-35}
+17        ListNode slow = head;
+18        ListNode fast = head;
+19        ListNode prev = null;
+20
+21        while (fast != null && fast.next != null) {
+22            prev = slow;
+23            slow = slow.next;
+24            fast = fast.next.next;
+25        }
+26
+27        prev.next = slow.next;
+28
+29        return head;
+30    }
+31}
