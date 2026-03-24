7a6934b89c0378cea269c8dfdc57c03042277b89
Ved Duhijod <65458338+vedduhijod@users.noreply.github.com>
2026-03-25 00:47:07 +0530

    Time: 5 ms (98.31%) | Memory: 44.1 MB (22.00%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,17 +1,20 @@
 class Solution {
-    public static boolean isIsomorphic(String s, String t) {
-    int[] mapS = new int[256];
-    int[] mapT = new int[256];
+   public static boolean isIsomorphic(String s, String t) {
+        int[] mapS = new int[256];
+        int[] mapT = new int[256];
 
-    for (int i = 0; i < s.length(); i++) {
-        if (mapS[s.charAt(i)] != mapT[t.charAt(i)]) {
-            return false;
+        for (int i = 0; i < s.length(); i++) {
+            char chS = s.charAt(i);
+            char chT = t.charAt(i);
+
+            if (mapS[chS] != mapT[chT]) {
+                return false;
+            }
+
+            mapS[chS] = i + 1;
+            mapT[chT] = i + 1;
         }
-        mapS[s.charAt(i)] = i + 1;
-        mapT[t.charAt(i)] = i + 1;
+
+        return true;
     }
-
-    return true;
 }
-}
-
