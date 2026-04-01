8907a25cfcb2da40e02b444187eeaefb24cea205
Ved Duhijod <65458338+vedduhijod@users.noreply.github.com>
2026-04-01 13:36:54 +0530

    Time: 2 ms (99.98%) | Memory: 46.3 MB (82.87%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,20 +1,20 @@
 class Solution {
     public int romanToInt(String s) {
-        java.util.Map<Character, Integer> map = new java.util.HashMap<>();
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
+        int[] values = new int[26];
+        values['I' - 'A'] = 1;
+        values['V' - 'A'] = 5;
+        values['X' - 'A'] = 10;
+        values['L' - 'A'] = 50;
+        values['C' - 'A'] = 100;
+        values['D' - 'A'] = 500;
+        values['M' - 'A'] = 1000;
 
         int total = 0;
 
         for (int i = 0; i < s.length(); i++) {
-            int current = map.get(s.charAt(i));
+            int current = values[s.charAt(i) - 'A'];
 
-            if (i < s.length() - 1 && current < map.get(s.charAt(i + 1))) {
+            if (i < s.length() - 1 && current < values[s.charAt(i + 1) - 'A']) {
                 total -= current;
             } else {
                 total += current;
