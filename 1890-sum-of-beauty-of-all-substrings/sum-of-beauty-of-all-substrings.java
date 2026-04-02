9eeb008742dff69a2f461e5d437d66a2af367279
Ved Duhijod <65458338+vedduhijod@users.noreply.github.com>
2026-04-03 01:38:34 +0530

    Time: 59 ms (89.78%) | Memory: 44.3 MB (40.74%) - LeetSync

---
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int beautySum(String s) {
+        int n = s.length();
+        int totalBeauty = 0;
+
+                for (int i = 0; i < n; i++) {
+            int[] freq = new int[26];
+
+            for (int j = i; j < n; j++) {
+                freq[s.charAt(j) - 'a']++;
+
+                int maxFreq = 0;
+                int minFreq = Integer.MAX_VALUE;
+
+                for (int f : freq) {
+                    if (f > 0) {
+                        maxFreq = Math.max(maxFreq, f);
+                        minFreq = Math.min(minFreq, f);
+                    }
+                }
+
+                totalBeauty += (maxFreq - minFreq);
+            }
+        }
+
+        return totalBeauty;
+    }}
