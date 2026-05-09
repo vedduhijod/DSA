581272714c3f03d0a7ab1f56aa340e7ff410a74a
Ved Duhijod <65458338+vedduhijod@users.noreply.github.com>
2026-05-10 02:27:15 +0530

    [225] [Time Beats: 100.00%] [Memory Beats: 42.13%] - LeetPush

--- Implement-Stack-using-Queues.java
+++ Implement-Stack-using-Queues.java
@@ -1,45 +1,36 @@
-1class MyStack {
-2    static class Node{
-3        int data;
-4        Node next;
-5        Node(int data){
-6            this.data = data;
-7            this.next = null;
-8        }
-9    }
-10    private Node top;
-11
-12    public MyStack() {
-13        this.top = null;
-14    }
-15    
-16    public void push(int x) {
-17        Node newNode = new Node(x);
-18        newNode.next = top;
-19        top = newNode;
+1import java.util.LinkedList;
+2import java.util.Queue;
+3
+4class MyStack {
+5
+6    Queue<Integer> q = new LinkedList<>();
+7
+8    // Push element onto stack
+9    public void push(int x) {
+10
+11        int size = q.size();
+12
+13        // Add new element
+14        q.add(x);
+15
+16        // Rotate previous elements
+17        for (int i = 0; i < size; i++) {
+18            q.add(q.remove());
+19        }
 20    }
-21    
-22    public int pop() {
-23        int popped = top.data;
-24        top = top.next;
-25
-26        return popped;
-27    }
-28    
-29    public int top() {
-30      return top.data;  
-31    }
-32    
+21
+22    // Removes top element
+23    public int pop() {
+24        return q.remove();
+25    }
+26
+27    // Returns top element
+28    public int top() {
+29        return q.peek();
+30    }
+31
+32    // Check if empty
 33    public boolean empty() {
-34        return top == null;
+34        return q.isEmpty();
 35    }
 36}
-37
-38/**
-39 * Your MyStack object will be instantiated and called as such:
-40 * MyStack obj = new MyStack();
-41 * obj.push(x);
-42 * int param_2 = obj.pop();
-43 * int param_3 = obj.top();
-44 * boolean param_4 = obj.empty();
-45 */
