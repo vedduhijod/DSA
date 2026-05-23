2e730242e4e88617abc2889afe8c865308d32e27
Ved Duhijod <65458338+vedduhijod@users.noreply.github.com>
2026-05-23 19:03:09 +0530

    Time: 16 ms (92.76%) | Memory: 45.5 MB (65.57%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -2,50 +2,38 @@ class Solution {
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
 
-        int n = nums.length;
-        int closest = nums[0] + nums[1] + nums[2];
+        int minDiff = Integer.MAX_VALUE;
+        int ans = 0;
 
-        for (int i = 0; i < n - 2; i++) {
+        for (int i = 0; i < nums.length - 2; i++) {
 
-            // Skip duplicate starting values
             if (i > 0 && nums[i] == nums[i - 1])
                 continue;
 
-            // Minimum possible sum with current i
-            int minSum = nums[i] + nums[i + 1] + nums[i + 2];
-            if (minSum > target) {
-                if (Math.abs(minSum - target) < Math.abs(closest - target))
-                    closest = minSum;
-                break;
-            }
-
-            // Maximum possible sum with current i
-            int maxSum = nums[i] + nums[n - 2] + nums[n - 1];
-            if (maxSum < target) {
-                if (Math.abs(maxSum - target) < Math.abs(closest - target))
-                    closest = maxSum;
-                continue;
-            }
-
             int left = i + 1;
-            int right = n - 1;
+            int right = nums.length - 1;
 
             while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
 
-                if (Math.abs(sum - target) < Math.abs(closest - target))
-                    closest = sum;
+                int sum = nums[i] + nums[left] + nums[right];
+                int diff = Math.abs(sum - target);
+
+                if (diff < minDiff) {
+                    minDiff = diff;
+                    ans = sum;
+                }
 
                 if (sum == target)
                     return target;
 
-                if (sum < target)
+                else if (sum < target)
                     left++;
+
                 else
                     right--;
             }
         }
 
-        return closest;
+        return ans;
     }
 }
