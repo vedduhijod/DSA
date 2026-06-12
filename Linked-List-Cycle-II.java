a2072f48db7abc9f00efe250607c83b81bae4072
Ved Duhijod <65458338+vedduhijod@users.noreply.github.com>
2026-06-12 18:54:19 +0530

    [142] [Time Beats: 100.00%] [Memory Beats: 29.14%] - LeetPush

--- Linked-List-Cycle-II.java
+++ Linked-List-Cycle-II.java
@@ -1,34 +1,30 @@
-1/**
-2 * Definition for singly-linked list.
-3 * class ListNode {
-4 *     int val;
-5 *     ListNode next;
-6 *     ListNode(int x) {
-7 *         val = x;
-8 *         next = null;
-9 *     }
-10 * 
-11 */
-12 import java.util.HashSet;
-13import java.util.Set;
+1public class Solution {
+2    public ListNode detectCycle(ListNode head) {
+3        if (head == null || head.next == null) {
+4            return null;
+5        }
+6
+7        ListNode slow = head;
+8        ListNode fast = head;
+9
+10        // Step 1: Detect cycle
+11        while (fast != null && fast.next != null) {
+12            slow = slow.next;
+13            fast = fast.next.next;
 14
-15public class Solution {
-16    public ListNode detectCycle(ListNode head) {
-17
-18        Set<ListNode> visited = new HashSet<>();
-19
-20        ListNode current = head;
-21
-22        while (current != null) {
+15            if (slow == fast) {
+16                // Step 2: Find cycle start
+17                ListNode ptr = head;
+18
+19                while (ptr != slow) {
+20                    ptr = ptr.next;
+21                    slow = slow.next;
+22                }
 23
-24            if (visited.contains(current)) {
-25                return current;   // cycle start node
-26            }
+24                return ptr;
+25            }
+26        }
 27
-28            visited.add(current);
-29            current = current.next;
-30        }
-31
-32        return null;
-33    }
-34}
+28        return null;
+29    }
+30}
