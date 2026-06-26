e707a69acf70fd8623d41bb504762dbf827ec82f
Ved Duhijod <65458338+vedduhijod@users.noreply.github.com>
2026-06-26 12:01:47 +0530

    [234] [Time Beats: 43.97%] [Memory Beats: 54.56%] - LeetPush

--- Palindrome-Linked-List.java
+++ Palindrome-Linked-List.java
@@ -10,45 +10,38 @@
 10 */
 11class Solution {
 12    public boolean isPalindrome(ListNode head) {
-13
-14        if(head == null || head.next == null)
-15            return true;
-16
-17        // Step 1: Find middle
-18        ListNode slow = head;
-19        ListNode fast = head;
-20
-21        while(fast.next != null && fast.next.next != null){
-22            slow = slow.next;
-23            fast = fast.next.next;
-24        }
-25
-26        // Step 2: Reverse second half
-27        ListNode second = reverse(slow.next);
-28        ListNode first = head;
-29
-30        // Step 3: Compare
-31        while(second != null){
-32            if(first.val != second.val)
-33                return false;
-34
-35            first = first.next;
-36            second = second.next;
-37        }
+13        if(head == null || head.next == null)
+14        return true;
+15
+16        ListNode slow = head;
+17        ListNode fast = head;
+18
+19        while(fast != null && fast.next != null){
+20            slow = slow.next;
+21            fast = fast.next.next;
+22        }
+23        
+24        ListNode second = reverse(slow);
+25        ListNode first = head;
+26
+27        while(second != null){
+28            if(first.val != second.val){
+29                return false;
+30            }
+31            first = first.next;
+32            second = second.next;
+33        }
+34        return true;
+35    }
+36    private ListNode reverse(ListNode head){
+37        ListNode prev = null;
 38
-39        return true;
-40    }
-41
-42    private ListNode reverse(ListNode head){
-43        ListNode prev = null;
-44
-45        while(head != null){
-46            ListNode next = head.next;
-47            head.next = prev;
-48            prev = head;
-49            head = next;
-50        }
-51
-52        return prev;
-53    }
-54}
+39        while(head != null){
+40            ListNode next = head.next;
+41            head.next = prev;
+42            prev = head;
+43            head = next;
+44        }
+45        return prev;
+46    }
+47}
