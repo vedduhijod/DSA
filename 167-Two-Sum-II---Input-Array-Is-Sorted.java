16a687b0e668c2431c971efeba233f21bd265efd
Ved Duhijod <65458338+vedduhijod@users.noreply.github.com>
2026-07-19 19:37:54 +0530

    [167] [Time Beats: 95.57%] [Memory Beats: 42.38%] - LeetPush

---
+++ 167-Two-Sum-II---Input-Array-Is-Sorted.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int i = 0;
+        int j = numbers.length - 1;
+        if (numbers.length == 0) {
+            return new int[] { -1, -1 };
+        }
+
+        while (i < j) {
+            int res = numbers[i] + numbers[j];
+            if (res == target) {
+                return new int[] { i + 1, j + 1 };
+            } else if (res > target) {
+                j--;
+            } else {
+                i++;
+            }
+            res = numbers[i] + numbers[j];
+        }
+        return new int[] { -1, -1 };
+    }
+}
