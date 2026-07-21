f06e81b27251d714112afac119a06d5b1e625179
Ved Duhijod <65458338+vedduhijod@users.noreply.github.com>
2026-07-21 14:01:02 +0530

    [1047] [Time Beats: 28.83%] [Memory Beats: 75.70%] - LeetPush

---
+++ 1047-Remove-All-Adjacent-Duplicates-In-String.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for(char c : s.toCharArray()){
+            if(!st.isEmpty() && st.peek() == c){
+                st.pop();
+            }else{
+                st.push(c);
+            }
+        }
+        StringBuilder res = new StringBuilder();
+
+        while(!st.isEmpty()){
+            res.append(st.pop());
+        }
+        return res.reverse().toString();
+    }
+}
