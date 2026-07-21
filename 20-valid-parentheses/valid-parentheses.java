52e89c32c90008759233dd49df8c1505775c124f
Ved Duhijod <65458338+vedduhijod@users.noreply.github.com>
2026-07-21 17:59:28 +0530

    Time: 2 ms (98.32%) | Memory: 43.1 MB (73.66%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -5,11 +5,20 @@ class Solution {
         Stack<Character> st = new Stack<>();
 
         for (char c : s.toCharArray()) {
-            if (c == '(') st.push(')');
-            else if (c == '{') st.push('}');
-            else if (c == '[') st.push(']');
-            else {
-                if (st.isEmpty() || st.pop() != c) {
+            if(c == '{' || c == '[' || c == '('){
+                st.push(c);
+            }else{
+                if (st.isEmpty()) {
+                    return false;
+                }
+                char top = st.peek();   // Look at the top
+
+                if ((c == ')' && top == '(') ||
+                    (c == '}' && top == '{') ||
+                    (c == ']' && top == '[')) {
+
+                    st.pop();           // Remove only if it matches
+                } else {
                     return false;
                 }
             }
