07d1485a4463ddeba179134977972b7845ed98f5
Ved Duhijod <65458338+vedduhijod@users.noreply.github.com>
2026-07-29 01:59:00 +0530

    [1189] [Time Beats: 14.81%] [Memory Beats: 6.47%] - LeetPush

---
+++ 1189-Maximum-Number-of-Balloons.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char ch : text.toCharArray()){
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        int b = map.getOrDefault('b', 0);
+        int a = map.getOrDefault('a', 0);
+        int l = map.getOrDefault('l', 0) / 2;
+        int o = map.getOrDefault('o', 0) / 2;
+        int n = map.getOrDefault('n', 0);
+        
+        int min = b;
+        if (a < min) min = a;
+        if (l < min) min = l;
+        if (o < min) min = o;
+        if (n < min) min = n;
+
+        return min;
+    }
+}
