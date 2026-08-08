88ddcff93c7ff988cc4b94b791870947b8736533
Ved Duhijod <65458338+vedduhijod@users.noreply.github.com>
2026-08-09 03:44:38 +0530

    [153] [Time Beats: 100.00%] [Memory Beats: 94.95%] - LeetPush

---
+++ 153-Find-Minimum-in-Rotated-Sorted-Array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        int result = -1;
+
+        while(low <= high){
+            int guess = (low + high) / 2;
+            if(nums[guess] > nums[nums.length - 1]){
+                low = guess + 1;
+            }else{
+                result = guess;
+                high = guess - 1;
+            }
+        }
+        return nums[result];
+    }
+}
